1c98a8de3524274fec3fec8359297a12d6d9e742
oskarrebermark <oskar.rebermark@gmail.com>
2025-01-27 21:33:13 +0100

    lots of changes

--- functions.js
+++ functions.js
@@ -1,8 +1,21 @@
 function openProject(projectName) {
+    // 
     var i;
     var x = document.getElementsByClassName("project");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
     document.getElementById(projectName).style.display = "block";
-  }
+    
+    // Remove 'active' class from all tabs
+    var tabs = document.getElementsByClassName("tab");
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove("activeP");
+    }
+
+    // Add 'active' class to the clicked tab
+    var clickedTab = document.querySelector(`[onclick="openProject('${projectName}')"]`);
+    if (clickedTab) {
+        clickedTab.classList.add("activeP");
+    }
+}
