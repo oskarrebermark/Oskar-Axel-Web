0ae3549810988392bebc401eeae9ff377c8dc816
oskarrebermark <oskar.rebermark@gmail.com>
2025-01-27 14:48:25 +0100

    added script

--- functions.js
+++ functions.js
@@ -1,1 +1,8 @@
-
+function openProject(project) {
+    var i;
+    var x = document.getElementsByClassName("project");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(project).style.display = "block";
+}
