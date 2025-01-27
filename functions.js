348ecb9eb36407e90fa5b6d78c16049a57b88ce5
oskarrebermark <oskar.rebermark@gmail.com>
2025-01-27 15:19:42 +0100

    update ui

--- functions.js
+++ functions.js
@@ -1,8 +1,8 @@
-function openProject(project) {
+function openCity(cityName) {
     var i;
     var x = document.getElementsByClassName("project");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
-    document.getElementById(project).style.display = "block";
-}
+    document.getElementById(cityName).style.display = "block";
+  }
